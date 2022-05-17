79a4c645cf4bfbc4fbebb03b4582a0d39bf5c0d0
MoonysKR <dytjd297@gmail.com>
2022-05-17 22:11:30 +0900

    220517_movielens_data

--- data/test_ratings.py
+++ data/test_ratings.py
@@ -1,10 +1,11 @@
 import sys
+import random
 
 sys.stdin = open('ratings.dat', encoding='ISO-8859-1')
 
 data = []
-
 lst = []
+arr = []
 
 for i in range(1000209):
     info = input()
@@ -21,20 +22,41 @@ for i in range(1000209):
         ]
 
     }
+    # 표본 추출
+#     lst += [dic]
+
+# new_lst = random.sample(lst, 100000)
+# # print(new_lst)
+
+# for i in range(100000):
+#     dic = new_lst[i]
+#     if dic['id'] in arr:
+#         for dat in data:
+#             if dat['id'] == dic['id']:
+#                 dat['results'] += dic['results']
+#     else:
+#         arr += [dic['id']]
+#         data += [dic]
     
+    # print(arr)
+
+# print(data)
+
+    # 기존 코드
     if user_id in lst:
         for dat in data:
             if dat['id'] == user_id:
                 dat['results'] += dic['results']
     else:
-        lst += dic['id']
+        lst += [dic['id']]
         data += [dic]
-            
-    print(data)
+    
+    # print(lst)
+    # print(data)
     
 # print(data)
 
-# import json
+import json
 
-# with open('ratings.json', 'w') as f:
-#     json_string = json.dump(data, f, indent=2)
+with open('ratings.json', 'w') as f:
+    json_string = json.dump(data, f, indent=2)
